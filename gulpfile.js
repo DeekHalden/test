b493f04d59f442ff941f2e4f8aa800ec9113c115
deekhalden <iliy4@ua.fm>
2016-10-20 20:19:50 +0300

    better

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,24 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
-var reload      = browserSync.reload;
+var sass        = require('gulp-sass');
 
-// Static server
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
 
-
-gulp.task('serve', function () {
-
-    // Serve files from the root of this project
     browserSync.init({
-        server: {
-            baseDir: "./"
-        }
+        server: "./"
     });
 
-    gulp.watch(["*.html",'css/*.css']).on("change", reload);
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
 });
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
